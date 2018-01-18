9f3cba279af042a4fce68f512866b0c3c1c29bcc
ligar mugi syahid <mugisyahid@gmail.com>
2018-01-18 07:05:48 +0700

    login form

--- src/app/dashboard/login/login.component.ts
+++ src/app/dashboard/login/login.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AppConfig} from '../../config/app.config';
+import {LoggerService} from '../../core/logger.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
